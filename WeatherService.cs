f773ab32a6411544a4b7aaa60f21dc12a03b6656
e2203080 <e2203080@edu.vamk.fi>
2024-11-22 18:10:41 +0200

    API building- under contruction

--- WeatherService.cs
+++ WeatherService.cs
@@ -1,5 +1,6 @@
 using System.Net.Http;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace WeatherApp
@@ -10,17 +11,53 @@ namespace WeatherApp
 
         public static async Task<(double MinTemp, double MaxTemp, double Rain)> GetWeatherSummary()
         {
-            double latitude = 35.6895;  // Default latitude
-            double longitude = 139.6917;  // Default longitude
+            double latitude = 63.096;
+            double longitude = 21.62;
 
-            string apiUrl = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=auto";
-            var response = await _httpClient.GetAsync(apiUrl);
-            response.EnsureSuccessStatusCode();
+            string apiUrl = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,precipitation,rain,showers&hourly=temperature_2m,wind_speed_1000hPa&daily=weather_code,temperature_2m_max,temperature_2m_min,wind_speed_10m_max&timezone=auto&models=best_match";
 
-            var json = await response.Content.ReadAsStringAsync();
-            var weatherData = JsonSerializer.Deserialize<WeatherResponse>(json);
+            try
+            {
+                var response = await _httpClient.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
 
-            return (weatherData.Daily.Temperature2mMin[0], weatherData.Daily.Temperature2mMax[0], weatherData.Daily.PrecipitationSum[0]);
+                var json = await response.Content.ReadAsStringAsync();
+                var weatherData = JsonSerializer.Deserialize<WeatherResponse>(json);
+
+                double minTemp = weatherData.Daily.Temperature2mMin[0];
+                double maxTemp = weatherData.Daily.Temperature2mMax[0];
+                double precipitation = weatherData.Current.Precipitation;
+
+                return (minTemp, maxTemp, precipitation);
+            }
+            catch (HttpRequestException ex)
+            {
+                
+                return (0, 0, 0); 
+            }
         }
+    }
+
+    public class WeatherResponse
+    {
+        public CurrentWeather Current { get; set; }
+        public DailyWeather Daily { get; set; }
+    }
+
+    public class CurrentWeather
+    {
+        [JsonPropertyName("temperature_2m")]
+        public double Temperature2m { get; set; }
+
+        public double Precipitation { get; set; }
+    }
+
+    public class DailyWeather
+    {
+        [JsonPropertyName("temperature_2m_min")]
+        public double[] Temperature2mMin { get; set; }
+
+        [JsonPropertyName("temperature_2m_max")]
+        public double[] Temperature2mMax { get; set; }
     }
 }
