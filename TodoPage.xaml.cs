7318bd610bc8dfe00a68f84091808d312d441f69
e2203080 <e2203080@edu.vamk.fi>
2024-11-25 13:25:14 +0200

    to do update, assignments are kept.

--- TodoPage.xaml.cs
+++ TodoPage.xaml.cs
@@ -1,4 +1,4 @@
-using Microsoft.Maui.Controls;
+/*using Microsoft.Maui.Controls;
 using System.Collections.ObjectModel;
 
 namespace WeatherApp
@@ -28,4 +28,60 @@ namespace WeatherApp
         public string Task { get; set; }
         public string DueDate { get; set; }
     }
+}*/
+
+
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+
+namespace WeatherApp
+{
+    public partial class TodoPage : ContentPage
+    {
+        private ObservableCollection<TodoItem> todoItems = new ObservableCollection<TodoItem>();
+
+        public TodoPage()
+        {
+            InitializeComponent();
+            LoadTasks();
+        }
+
+        private void LoadTasks()
+        {
+            todoItems.Clear();
+            foreach (var task in TodoService.GetPendingTasks())
+            {
+                todoItems.Add(task);
+            }
+            todoListView.ItemsSource = todoItems;
+        }
+
+        private async void OnAddTaskClicked(object sender, EventArgs e)
+        {
+            string result = await DisplayPromptAsync("New Task", "Enter task description:");
+            if (!string.IsNullOrWhiteSpace(result))
+            {
+                TodoService.AddTask(result);
+                LoadTasks();
+            }
+        }
+
+        private void OnCompleteTaskClicked(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.BindingContext is TodoItem task)
+            {
+                TodoService.MarkTaskComplete(task.Task);
+                LoadTasks();
+            }
+        }
+
+        private async void OnTaskTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item is TodoItem task)
+            {
+                await DisplayAlert("Task Details", $"Task: {task.Task}\nDue: {task.DueDate}", "OK");
+            }
+        }
+    }
 }
+
--- TodoService.cs
+++ TodoService.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 
 namespace WeatherApp
 {
@@ -10,4 +10,41 @@ namespace WeatherApp
 
         public static void AddTask(string task) => Todos.Add(task);
     }
+}*/
+
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WeatherApp
+{
+    public static class TodoService
+    {
+        private static readonly List<TodoItem> Todos = new List<TodoItem>();
+
+        public static int GetTasksCount() => Todos.Count(t => !t.IsCompleted);
+
+        public static void AddTask(string task) =>
+            Todos.Add(new TodoItem { Task = task, DueDate = DateTime.Now.ToString("MMM dd"), IsCompleted = false });
+
+        public static List<TodoItem> GetPendingTasks() =>
+            Todos.Where(t => !t.IsCompleted).ToList();
+
+        public static void MarkTaskComplete(string task)
+        {
+            var todo = Todos.FirstOrDefault(t => t.Task == task);
+            if (todo != null)
+            {
+                todo.IsCompleted = true;
+            }
+        }
+    }
+
+    public class TodoItem
+    {
+        public string Task { get; set; }
+        public string DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
 }
+
