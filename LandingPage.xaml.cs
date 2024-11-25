65ce827cbaefc281302f01a0a3083bebb96e0d3b
e2203080 <e2203080@edu.vamk.fi>
2024-11-25 13:39:24 +0200

    landing page view update

--- LandingPage.xaml.cs
+++ LandingPage.xaml.cs
@@ -1,4 +1,4 @@
-using Microsoft.Maui.Controls;
+/*using Microsoft.Maui.Controls;
 
 namespace WeatherApp
 {
@@ -37,4 +37,51 @@ namespace WeatherApp
             await Navigation.PushAsync(new TodoPage());
         }
     }
+}*/
+
+using Microsoft.Maui.Controls;
+
+namespace WeatherApp
+{
+    public partial class LandingPage : ContentPage
+    {
+        public LandingPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            LoadSummary();
+        }
+
+        private async void LoadSummary()
+        {
+            // Load basic weather data
+            try
+            {
+                var weatherData = await WeatherService.GetWeatherSummary();
+                weatherSummaryLabel.Text = $"Min Temp: {weatherData.MinTemp}°C, Max Temp: {weatherData.MaxTemp}°C, Rain: {weatherData.Rain} mm";
+            }
+            catch
+            {
+                weatherSummaryLabel.Text = "Unable to load weather data.";
+            }
+
+            // Load todo summary
+            todoSummaryLabel.Text = $"Tasks Today: {TodoService.GetTasksCount()}";
+        }
+
+        private async void OnWeatherPageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void OnTodoPageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TodoPage());
+        }
+    }
 }
+
